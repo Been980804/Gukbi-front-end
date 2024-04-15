0b84803c48d4f46937716a215dbee7502fe51f93
dlsghths <dlsghths@gmail.com>
2024-04-16 01:49:09 +0900

    사이드바 메뉴 선택마다 화면이동이 되도록 기능 수정

--- src/views/manage/InquiryListView.vue
+++ src/views/manage/InquiryListView.vue
@@ -94,7 +94,7 @@ export default {
 
   // DOM이 만들어진 후 실행
   mounted() {
-    this.$refs.mgrSidebar.getLargeMenu(9);
+    this.$refs.mgrSidebar.setCurrentMenu(9, this.$route.path, this.$route.query.menuNo);
   },
 
   methods: {
