6a364232eae7e5357ec114b9b007ac0240958b13
dlsghths <dlsghths@gmail.com>
2024-04-18 13:12:03 +0900

    문의사항 답변 등록 로직 추가

--- src/views/manage/InquiryDetailView.vue
+++ src/views/manage/InquiryDetailView.vue
@@ -46,7 +46,7 @@
           <div class="m_show_box_title">문의사항 답변</div>
 
           <div class="inline_blank24"></div>
-          <textarea class="answer_box" style="width: 100%;" rows="10"></textarea>
+          <textarea class="answer_box" style="width: 100%;" rows="10" ref="answer"></textarea>
 
           <div class="inline_blank24"></div>
 
@@ -94,11 +94,10 @@ export default {
       this.inquiryNo = sessionStorage.getItem("inquiryNo");
     }
 
-    api.get(`/manage/board/inquiry/inquiryInfo/${this.inquiryNo}}`)
+    api.get(`/manage/board/inquiry/inquiryInfo/${this.inquiryNo}`)
       .then(res => {
         if(res.common.res_code == 200 && res.data.inquiryInfo != '') {
           this.inquiryInfo = res.data.inquiryInfo;
-          console.log(this.inquiryInfo)
         } else {
           console.log("BookDetailView book/inquiryInfo 응답실패");
         }
@@ -120,17 +119,20 @@ export default {
     },
 
     regist() { // 답변등록
-      api.get(`/manage/board/inquiry/inquiryInfo/${this.inquiryNo}}`)
-      .then(res => {
-        if(res.common.res_code == 200 && res.data.inquiryInfo != '') {
-          this.inquiryInfo = res.data.inquiryInfo;
-          console.log(this.inquiryInfo)
-        } else {
-          console.log("BookDetailView book/inquiryInfo 응답실패");
-        }
-      })
+      let sqlData = new Map();
+      sqlData.set("answer", this.$refs.answer.value);
+      sqlData.set("inquiryNo", this.inquiryNo);
+
+      api.put(`/manage/board/inquiry/answer`, {params: Object.fromEntries(sqlData)})
+        .then(res => {
+          if(res.common.res_code == 200 && res.data.answer == 1) {
+            this.$router.push({path: 'MgrInquiryList'});
+          } else {
+            console.log("InquiryDetailView inquiry/answer 응답실패");
+          }
+        })
     }
-  },
+  }
 }
 </script>
 <style>
