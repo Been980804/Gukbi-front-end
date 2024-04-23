beb9b5ec38501965961524f3f8a30920ac100bba
dlsghths <dlsghths@gmail.com>
2024-04-23 23:17:59 +0900

    메인화면 도서상세정보 받아오는 기능 추가

--- src/views/common/DetailView.vue
+++ src/views/common/DetailView.vue
@@ -1,8 +1,39 @@
 <template>
   <div>
-    
+    {{ isbn }}
   </div>
 </template>
-<style>
+<script>
+import api from "@/api/axios";
+export default {
+  data() {
+    return {
+      isbn: this.$route.params.isbn,
+      bookInfo: {}
+    }
+  },
 
+  created() {
+    this.getBookInfo();
+  },
+
+  methods: {
+    getBookInfo() { // 도서 정보 가져오기
+      api.get(`/main/bookInfo/${this.isbn}`)
+        .then(res => {
+          if(res.common.res_code == 200 && res.data.book_no != '') {
+            this.bookInfo = res.data.bookInfo;
+            console.log(this.bookInfo);
+            // this.sumCategory();
+          } else {
+            console.log("DetailView main/bookInfo 응답실패");
+          }
+        })
+      // this.getDescript(event.target.value);
+    },
+  }
+}
+</script>
+<style>
+@import "../../assets/css/main/main.css";
 </style>
