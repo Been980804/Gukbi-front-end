dd751cae1198a1afdcb0a0f77d02bdd1ed1b2e64
dlsghths <dlsghths@gmail.com>
2024-04-26 08:21:15 +0900

    footer view 공지사항 리스트에 따른 ui 수정

--- src/components/common/FooterView.vue
+++ src/components/common/FooterView.vue
@@ -15,40 +15,15 @@
 				</div>
 				<div class="notice_contents">
 					<ul>
-						<li>
+						<li v-for="noti in notiList" :key="noti">
 							<a href="#">
-								<div class="noti">국립중앙도서관 우편복사 서비스 지연 안내</div>
-								<div class="date">2024-02-22</div>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<div class="noti">학술자료부터 문화예술자료까지 다양한 온라인 콘텐츠 제공</div>
-								<div class="date">2024-02-22</div>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<div class="noti">2024년 근하신년</div>
-								<div class="date">2024-02-22</div>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<div class="noti">제12차 「해설이 있는 K-컬처」 프로그램 안내</div>
-								<div class="date">2024-02-22</div>
-							</a>
-						</li>
-						<li>
-							<a href="#">
-								<div class="noti">국립중앙도서관 「열린마당」 방문 인증 이벤트 당첨자 발표</div>
-								<div class="date">2024-02-22</div>
+								<div class="noti">{{ noti.noti_title }}</div>
+								<div class="date">{{ noti.mod_date }}</div>
 							</a>
 						</li>
 					</ul>
 				</div>
 				</div>
-				<!-- recent -->
 				<div class="recent">
 			</div>
 		</div>
@@ -80,7 +55,6 @@ export default {
         .then(res => {
           if (res.common.res_code == 200) { // 응답성공
             this.notiList = res.data.notiList;
-						console.log(this.notiList);
           } else { // 응답실패
             console.log("BookListView book/bookList 응답실패");
           }
