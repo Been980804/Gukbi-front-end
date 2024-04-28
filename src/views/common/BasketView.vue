e9c5b06322f86defd0a60a2ea0dcda3a35e18d02
dlsghths <dlsghths@gmail.com>
2024-04-28 10:47:07 +0900

    책바구니 화면에서 사용자 로그아웃시 메인화면으로 이동 처리

--- src/views/common/BasketView.vue
+++ src/views/common/BasketView.vue
@@ -4,3 +4,20 @@
     책바구니 화면
   </div>
 </template>
+<script>
+import { useUserStore } from '@/stores/user.js';
+export default {
+  data() {
+    return {
+      user: useUserStore().getUser  
+    }
+  },
+
+  created() {
+    if(this.user.mem_id == null || undefined || '') {
+      this.$router.push({ name: 'Main' });
+    }
+    console.log("aaaaaaaa");
+  },
+}
+</script>
