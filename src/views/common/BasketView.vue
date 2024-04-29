f59dba8f84769736c7cf97b70225e65bc6c5e190
dlsghths <dlsghths@gmail.com>
2024-04-29 11:46:43 +0900

    책바구니 리스트 조회 기능 추가

--- src/views/common/BasketView.vue
+++ src/views/common/BasketView.vue
@@ -5,11 +5,13 @@
   </div>
 </template>
 <script>
+import api from "@/api/axios";
 import { useUserStore } from '@/stores/user.js';
 export default {
   data() {
     return {
-      user: useUserStore().getUser  
+      user: useUserStore().getUser,
+      basketList: {}
     }
   },
 
@@ -17,7 +19,22 @@ export default {
     if(this.user.mem_id == null || undefined || '') {
       this.$router.push({ name: 'Main' });
     }
-    console.log("aaaaaaaa");
+    
+    // 사용자 책바구니 내역 가져오기
+    this.getBasket();
+  },
+
+  methods: {
+    getBasket() {
+      api.get(`/main/bookInfo/basketList/${this.user.mem_no}`)
+        .then(res => {
+          if (res.common.res_code == 200) { // 응답성공
+            this.basketList = res.data.basketList;
+          } else { // 응답실패
+            console.log("BookListView book/bookCount 응답실패");
+          }
+        })
+    }
   },
 }
 </script>
