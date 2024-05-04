3e094f1a43cd0459b34a6af9c791c833c73d408d
dlsghths <dlsghths@gmail.com>
2024-05-04 18:51:14 +0900

    카테고리 팝업 ui 수정

--- src/components/common/CategoryP.vue
+++ src/components/common/CategoryP.vue
@@ -2,11 +2,21 @@
   <div class="modal">
     <div class="modal_main">
       <div class="modal_body">
-        
+        <div class="modal_title">도서 전체 목록</div>
+        <div class="modal_title_line_box"></div>
+        <input type="radio" v-model="category" value="자기계발">자기계발
+        <input type="radio" v-model="category" value="기타">자기계발
       </div>
     </div>
   </div>
 </template>
-<style>
-
-</style>
+<script>
+export default {
+  data() {
+    return {
+      category: null
+    }
+  }
+}
+</script>
+<style src="../../assets/css/common/index.css"></style>
