08ec16e02c1b5f9935f4604303ee25dea46fe378
dlsghths <dlsghths@gmail.com>
2024-05-04 20:27:36 +0900

    카테고리 목록 팝업 ui 수정

--- src/components/common/CategoryP.vue
+++ src/components/common/CategoryP.vue
@@ -2,10 +2,11 @@
   <div class="modal">
     <div class="modal_main">
       <div class="modal_body">
-        <div class="modal_title">도서 전체 목록</div>
+        <div class="modal_title">관심분야 카테고리 목록</div>
         <div class="modal_title_line_box"></div>
-        <input type="radio" v-model="category" value="자기계발">자기계발
-        <input type="radio" v-model="category" value="기타">자기계발
+        <div v-for="prefer in categoryInfo" :key="prefer" style="font-size: 24px; margin: 8px 0px">
+          <input class="modal_input" type="radio" v-model="category" :value="`${prefer.pf_name}`">{{ prefer.pf_name }}
+        </div>
       </div>
     </div>
   </div>
@@ -15,7 +16,8 @@ import api from "@/api/axios";
 export default {
   data() {
     return {
-      category: null
+      category: "자기개발",
+      categoryInfo: null
     }
   },
 
@@ -28,7 +30,7 @@ export default {
       api.get(`/main/category`)
         .then(res => {
           if (res.common.res_code == 200) { // 응답성공
-            console.log(res.data.category);
+            this.categoryInfo = res.data.category;
           } else { // 응답실패
             console.log("CategoryP user/category 응답실패");
           }
